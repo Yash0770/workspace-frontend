84423c3ead69121d52fd308e529f14a0429e7c4e
Yash <yashsolanki0728@gmail.com>
2026-03-31 16:49:30 +0530

    Add sidebarSections data and refactor Sidebar component for dynamic rendering

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,28 +1,30 @@
 import Icon from "@/components/ui/Icon/Icon";
+import { sidebarSections } from "../../constants/dummyData";
 
 export default function Sidebar() {
   return (
     <aside className="w-64 bg-gray-50 text-gray-900 p-5 border-r border-gray-200">
-      <h2 className="lg:mt-3 text-base font-medium text-gray-500 mb-4 uppercase">
-        My projects
-      </h2>
+      {sidebarSections.map((section, index) => (
+        <div key={index} className="mb-6">
+          {/* Section Title */}
+          <h2 className="lg:mt-3 text-base font-medium text-gray-500 mb-4 uppercase">
+            {section.title}
+          </h2>
 
-      <nav className="space-y-3 text-base">
-        <div className="flex items-center gap-3 font-semibold hover:text-indigo-600 hover:bg-gray-100 px-4 py-2 rounded cursor-pointer">
-          <Icon name="agentIcon" size={20} />
-          <span>Agents</span>
+          {/* Items */}
+          <nav className="space-y-3 text-base">
+            {section.items.map((item, i) => (
+              <div
+                key={i}
+                className="flex items-center gap-3 font-semibold hover:text-indigo-600 hover:bg-gray-100 px-4 py-2 rounded cursor-pointer"
+              >
+                <Icon name={item.icon} size={20} />
+                <span>{item.label}</span>
+              </div>
+            ))}
+          </nav>
         </div>
-
-        <div className="flex items-center gap-3 font-semibold hover:text-indigo-600 hover:bg-gray-100 px-4 py-2 rounded cursor-pointer">
-          <Icon name="knowledgeIcon" size={20} />
-          <span>Knowledge Base</span>
-        </div>
-
-        <div className="flex items-center gap-3 font-semibold hover:text-indigo-600 hover:bg-gray-100 px-4 py-2 rounded cursor-pointer">
-          <Icon name="libraryIcon" size={20} />
-          <span>Library</span>
-        </div>
-      </nav>
+      ))}
     </aside>
   );
 }
---
+++ src/constants/dummyData.js
@@ -0,0 +1,24 @@
+export const sidebarSections = [
+  {
+    title: "My Projects",
+    items: [
+      { label: "Agents", icon: "agentIcon" },
+      { label: "AI Models", icon: "aiModelIcon" },
+      { label: "Library", icon: "libraryIcon" },
+    ],
+  },
+  {
+    title: "Orchestrator",
+    items: [
+      { label: "Published", icon: "publishIcon" },
+      { label: "Machines", icon: "shieldIcon" },
+      { label: "Queues", icon: "queueIcon" },
+      { label: "Triggers", icon: "trigerIcon" },
+      { label: "Jobs", icon: "jobIcon" },
+      { label: "Executions", icon: "executionIcon" },
+      { label: "Vault", icon: "vaultIcon" },
+      { label: "Knowledge base", icon: "knowledgeIcon" },
+      { label: "Key Store", icon: "keyStoreIcon" },
+    ],
+  },
+];
