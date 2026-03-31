9d39404089967c5d9d543ac67782d4c156713dce
Yash <yashsolanki0728@gmail.com>
2026-03-31 14:55:52 +0530

    Refactor MainLayout component structure for improved layout and styling; add container-1440 class for responsive design

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -3,13 +3,15 @@ import Header from "./Header";
 
 export default function MainLayout({ children }) {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      <div className="container-1440 flex flex-col flex-1">
+        <Header />
 
-      <div className="flex flex-1">
-        <Sidebar />
+        <div className="flex flex-1">
+          <Sidebar />
 
-        <main className="flex-1 bg-gray-50 p-6">{children}</main>
+          <main className="flex-1 bg-gray-50 p-4 md:p-6">{children}</main>
+        </div>
       </div>
     </div>
   );
